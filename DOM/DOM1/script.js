84252a8a8c4d15ba8c40d4ce9e6e317b55d3e480
Ousmane Diallo <ousmanediallo@Ousmanes-MacBook-Air.local>
2025-01-05 14:22:18 -0500

    DOM1

---
+++ DOM/DOM1/script.js
@@ -0,0 +1,9 @@
+var lastChild = document.getElementById('name')
+
+lastChild.innerHTML = 'Ousmane'
+lastChild.style.color = 'skyblue';
+lastChild.style.fontSize = '30px';
+const myDiv = document.querySelector("button");
+myDiv.style.backgroundColor = "yellow"; 
+myDiv.style.paddingRight = '40px'
+myDiv.style.border = 'none'
